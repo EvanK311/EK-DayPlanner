fb75fc5e452962383f6d329061b5fd5bfda3eb72
Evan K <EvanK311@gmail.com>
2020-09-17 21:00:48 -0700

    figuring out appends and loops

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 // TODO: Create one row with time, text area, and button using jquery.
 // layout for row/col format---
-{/* <div class="row time-block">
-      <div class="col-md-2 hour">
-        9AM
-      </div>
-      <textarea class="col-md-8">
+// {/* <div class="row time-block">
+//       <div class="col-md-2 hour">
+//         9AM
+//       </div>
+//       <textarea class="col-md-8">
 
-      </textarea>
-      <button class="col-md-2 saveBtn">
-        Save
-      </button>
-    </div> */}
+//       </textarea>
+//       <button class="col-md-2 saveBtn">
+//         Save
+//       </button>
+//     </div> */}
+
+var timeArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM",]
+var timeOfDay 
+var row = (".row")
+row.classList.add("time-block")
+
+$("#currentDay").text(moment().calendar());
+
+$(".container").append($(row))
 
 
-//TODO: Show time at top of the calendar, use moment.js to grab current time. set text of current time ptag with ID of currentDay 
+//TODO: Show time at top of the calendar, use moment.js to grab current time. set text of current time ptag with ID of currentDay(DONE) 
 
 //TODO: create row, create div, textarea, and a button, give them content, append them to row, then append row to page.(not repetitive method like loop.)
 
-//TODO:create array to hold these times (timeArray["9am","10am", "11am"])
+//TODO:create array to hold these times (timeArray["9am","10am", "11am"])(DONE)
 
 //TODO:apply elements to some content
 
@@ -30,4 +39,8 @@
 //TODO: add click event listener for save buttons 
 
 //TODO: grab value of text area and save to var. (need to be able to save text from the area that is same row as button.)
+
+//TODO: using localStorage.setItem, save text to local storage
+
+//TODO: when page is refreshed, saved data can be retrieved from local storage using localStorage.getItem and show them in correct text area.(set saved local)
 console.log(moment().format());
